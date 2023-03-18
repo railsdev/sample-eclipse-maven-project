3b4972d969ed2904f3d2a0e934fb63eb3f8ef4f2
Hardik Joshi <hardik@techverito.com>
2023-03-18 13:48:00 +0530

    basic eclipse maven project setup

---
+++ src/test/java/shoppingCart/CartTest.java
@@ -0,0 +1,13 @@
+package shoppingCart;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class CartTest {
+	@Test
+	public void test_cart_total_should_be_10() {
+		fail("test not implemented yet");
+	}
+
+}
